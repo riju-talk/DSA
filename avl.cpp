c131826c2302f6cf24122b65ab70558df2bfa87e
Phantom_Cloak18 <131883829+rijusmit22400@users.noreply.github.com>
2024-05-17 21:15:33 +0530

    init

---
+++ avl.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    //--------------------AVL Tree--------------------
+    return 0;
+}
---
+++ bt.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    //--------------------Binary Tree--------------------
+    return 0;
+}
